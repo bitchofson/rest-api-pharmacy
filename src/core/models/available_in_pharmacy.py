f4162586ef5bc52ab2377a4cfd4123e8d853499d
Vadim Remezov <aspowersan@protonmail.com>
2023-11-14 00:03:06 +0300

    Create migrastion for availble_in_pharmacy table: add cascade delete

--- src/core/models/available_in_pharmacy.py
+++ src/core/models/available_in_pharmacy.py
@@ -9,9 +9,9 @@ class AvailableInPharmacyORM(Base):
     __tablename__='available_in_pharmacy'
 
     pharmacy_id: Mapped[uuid.UUID] = mapped_column(
-        ForeignKey('pharmacy.id'),
+        ForeignKey('pharmacy.id', ondelete='CASCADE'),
         primary_key=True, )
     
     availability_id: Mapped[uuid.UUID] = mapped_column(
-        ForeignKey('availability.id'),
+        ForeignKey('availability.id', ondelete='CASCADE'),
         primary_key=True, )
